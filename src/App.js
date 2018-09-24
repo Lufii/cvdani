3c0425fb69676f689ca745a55012a0c07f60d58e
Dani Achim <dani_achim2005@yahoo.com>
2018-09-24 18:44:22 +0300

    added CSS,refining of content and display to follow

--- src/App.js
+++ src/App.js
@@ -41,8 +41,8 @@ class SVG extends Component {
         <path className='eyes1' d="m106.55 160.41-17.492-0.11004-31.402-46.545-32.857 46.529-17.359 0.0236 40.778-61.281 17.492 0.11004z" />
         <path className='eyes1' d="m392.56 160.51-17.492-0.11004-31.402-46.545-32.857 46.529-17.359 0.0236 40.778-61.281 17.492 0.11004z" />
         <flowRoot transform="matrix(.26458 0 0 .26458 0 82)" ><flowRegion><rect x="340" y="409.74" width="860" height="208.57"/></flowRegion><flowPara/></flowRoot>
-        <path className='eyes2' d="m100.02 163.51c0 15.517-3.3687 29.371-12.361 38.792-7.451 7.8057-19.49 12.584-30.122 12.584-11.731 0-25.013-4.1001-32.701-13.397-7.6878-9.2971-9.782-23.791-9.782-37.978 0-14.187 4.8417-28.823 12.53-38.12 7.6878-9.2971 18.222-13.255 29.953-13.255 11.731 0 24.498 4.6466 32.186 13.944 7.6878 9.2971 10.297 23.245 10.297 37.432z" />
-        <path className='eyes2' d="m386.06 162.63c0 15.517-3.3687 29.371-12.361 38.792-7.451 7.8057-19.49 12.584-30.122 12.584-11.731 0-25.013-4.1001-32.701-13.397-7.6878-9.2971-9.782-23.791-9.782-37.978 0-14.187 4.8417-28.823 12.53-38.12 7.6878-9.2971 18.222-13.255 29.953-13.255 11.731 0 24.498 4.6466 32.186 13.944 7.6878 9.2971 10.297 23.245 10.297 37.432z" />
+        <path className='eyes2' d="m102.91 153.3c0 11.201-2.2356 21.202-8.2032 28.002-4.9447 5.6346-12.934 9.0838-19.99 9.0838-7.7851 0-16.6-2.9597-21.702-9.6707-5.1019-6.7111-6.4917-17.174-6.4917-27.415 0-10.241 3.2131-20.806 8.3154-27.517 5.1019-6.7112 12.093-9.5682 19.878-9.5682s16.258 3.3542 21.36 10.066c5.1019 6.7112 6.8334 16.779 6.8334 27.02z" />
+        <path className='eyes2' d="m352.37 152.09c0 11.201-2.2356 21.202-8.2032 28.002-4.9448 5.6346-12.934 9.0838-19.99 9.0838-7.7851 0-16.6-2.9597-21.702-9.6707-5.1019-6.7111-6.4917-17.174-6.4917-27.415 0-10.241 3.2131-20.806 8.3154-27.517 5.1019-6.7112 12.093-9.5682 19.878-9.5682s16.258 3.3542 21.36 10.066c5.1019 6.7112 6.8334 16.779 6.8334 27.02z" />
        </g>
       </svg>
 </div>
@@ -75,6 +75,7 @@ class Ask extends Component{
     <p onClick={()=> {this.props.askChange('techs')}}>Techs</p>
     <p onClick={()=> {this.props.askChange('hobbies')}}>Hobbies</p>
     <p onClick={()=> {this.props.askChange('principles')}}>Principles</p>
+    <p onClick={()=> {this.props.askChange('jobhistory')}}>Job history</p>
     </div>
 <h1 className="accordion" onClick={()=> {this.setState({m2:1, m1:0})}}>Projects</h1>
     <h1 className="panel3 accordion" onClick={()=> {this.props.askChange('contact')}}>Contact</h1>
@@ -87,7 +88,7 @@ class Ask extends Component{
 <h1 className="accordion" onClick={()=> {this.setState({m2:1, m1:0})}}>Projects</h1>
 <div className="panel2">
   <p onClick={()=> {this.props.askChange('cv')}}>This page</p>
-  <p onClick={()=> {this.props.askChange('cildecor')}}>CILDecor</p>
+  <p onClick={()=> {this.props.askChange('cildecor')}}>CIL Decor</p>
   <p onClick={()=> {this.props.askChange('engulf')}}>Engulf</p>
   <p onClick={()=> {this.props.askChange('hw')}}>HalcyonWeave</p>
 </div>
@@ -157,9 +158,32 @@ class Say extends Component{
       <p>Design: significant video and image editing, color pallette selection and information structuring for the presentation website</p>
       </div>
     );
+    else if(this.props.sayVal==='engulf')
+    return(
+      <div>
+      <h3>Engulf - warehouse application</h3>
+      <p>Technical: MongoDB, Node, React.</p>
+      <p>Design: simple CRUD interface</p>
+      </div>
+    );
+    else if(this.props.sayVal==='hw')
+    return(
+      <div>
+      <h3>HalcyonWeave interface for freelancing projects</h3>
+      <p>Technical: React</p>
+      <p>Design: simple design</p>
+      </div>
+    );
+    else if(this.props.sayVal==='jobhistory')
+    return(
+      <div>
+      <h3>Jobs</h3>
+      <p>Book salesman, various construction jobs, lead generation specialist, explore musical side, writing, survey programmer</p>
+      </div>
+    );
     else
     return(
-      "ICESat-2 (Ice, Cloud, and land Elevation Satellite 2), part of NASA's Earth Observing System, is a satellite mission for measuring ice sheet elevation and sea ice thickness, as well as land topography and vegetation characteristics.[7] ICESat-2, a follow-on to the ICESat mission, was launched on 15 September 2018 from Vandenberg Air Force Base in California,[3] into a near-circular, near-polar orbit with an altitude of approximately 496 km (308 mi). It was designed to operate for three years and carry enough propellant for seven years.[8]"
+      null
     );
   }
 }
